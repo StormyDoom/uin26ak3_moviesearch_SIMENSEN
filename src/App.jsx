0cb5210af12556cd40d600520cc2a953be99d55b
StormyDoom <jesper.simensen19@gmail.com>
2026-02-27 14:02:25 +0100

    Arbeidskrav 3

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Movie from './pages/Movie'
+
+
+function App() {
+
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path=":movie" element={<Movie />} />
+    </Routes>
+  )
+}
+
+export default App
